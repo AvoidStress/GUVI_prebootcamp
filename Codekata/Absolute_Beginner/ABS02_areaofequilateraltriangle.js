eb7af1053929e457826cef7f2a07d005e27ecd91
Vijay-Anand-V <73172811+Vijay-Anand-V@users.noreply.github.com>
2020-11-06 20:11:54 +0530

    Create ABS02_areaofequilateraltriangle.js

---
+++ Codekata/Absolute_Beginner/ABS02_areaofequilateraltriangle.js
@@ -0,0 +1,44 @@
+/* Codekata-ABS02
+
+
+The area of an equilateral triangle is ¼(√3a2) where "a" represents a side of the triangle. You are provided with the side "a". Find the area of the equilateral triangle.
+
+Input Description:
+The side of an equilateral triangle is provided as the input.
+
+Output Description:
+Find the area of the equilateral triangle and print the answer up to 2 decimal places after rounding off.
+
+Sample Input :
+20
+
+Sample Output :
+173.21
+
+*/
+
+// Getting input via STDIN
+const readline = require("readline");
+
+const inp = readline.createInterface({
+  input: process.stdin
+});
+
+const userInput = [];
+
+inp.on("line", (data) => {
+  userInput.push(data);
+});
+
+inp.on("close", () => {
+  //start-here
+  //Your code goes here … replace the below line with your code logic 
+var a=parseFloat(userInput[0]);
+var ar=(.25)*(Math.sqrt(3))*(a*a)
+var area=ar.toFixed(2);
+
+
+  console.log(area);
+
+  //end-here
+});
